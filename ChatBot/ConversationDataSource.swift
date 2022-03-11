3e6e839998b1c67c7aaf84e7d266c448edd72c5e
Zachary Miller <zach_miller13@icloud.com>
2022-03-10 19:12:12 -0500

    Completion Part 5

--- ChatBot/ConversationDataSource.swift
+++ ChatBot/ConversationDataSource.swift
@@ -6,12 +6,16 @@ class ConversationDataSource {
     
     /// Add a new question to the conversation
     func add(question: String) {
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
         print("Asked to add question: \(question)")
         messageCount += 1
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
+        let message = Message(date: Date(), text: answer, type: .answer)
+        messages.append(message)
         print("Asked to add answer: \(answer)")
         messageCount += 1
     }
@@ -20,10 +24,16 @@ class ConversationDataSource {
     func messageAt(index: Int) -> Message {
         print("Asking for message at index \(index)")
         //return Message(date: Date(), text: "Hello, world!", type: .answer)
-        if index % 2 == 0 {
-            return Message(date: Date(), text: "Question \(index / 2)", type: .question)
-        } else {
-            return Message(date: Date(), text: "Answer \(index / 2)", type: .answer)
-        }
+//        if index % 2 == 0 {
+//            return Message(date: Date(), text: "Question \(index / 2)", type: .question)
+//        } else {
+//            return Message(date: Date(), text: "Answer \(index / 2)", type: .answer)
+//        }
+        return messages[index]
     }
+
+var messages = [Message]()
+    
+
 }
+
